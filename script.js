4f4a7e1aaacc74dd28fe815a75be1a3e112718ba
Igor <igoraraujobda@gmail.com>
2023-05-17 00:14:01 -0300

    realizando primeiras modificações

---
+++ script.js
@@ -0,0 +1,22 @@
+const menu = document.querySelector('#menu')
+const showHamburguer = () => {
+  if(window.innerWidth <= 600){
+    menu.classList = 'bi bi-list showMenu'
+} else {
+    menu.classList = 'hideMenu'
+  }
+}
+
+const handleMenu = () => {
+  const getNav = document.querySelector('#nav');
+  if(getNav.classList.contains('menuNavOff')){
+    getNav.classList.remove('menuNavOff');
+    getNav.classList.add('menuNavOn');
+  } else {
+    getNav.classList.remove('menuNavOn');
+    getNav.classList.add('menuNavOff');
+  }
+}
+  
+menu.addEventListener('click', handleMenu);
+window.addEventListener('resize', showHamburguer);
